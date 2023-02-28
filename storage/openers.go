3f89b9aec69897e6370325474183b374b104cfe6
Antony Jekov <antony.jekov@gmail.com>
2023-02-28 09:58:41 +0100

    adds nested dirs creation

--- storage/openers.go
+++ storage/openers.go
@@ -1,8 +1,10 @@
 package storage
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 type (
@@ -14,5 +16,10 @@ func (o FileOpener) OpenRead(path string) (io.ReadCloser, error) {
 }
 
 func (o FileOpener) OpenCreate(path string) (io.WriteCloser, error) {
+	dir := filepath.Dir(path)
+	if errMkDir := os.MkdirAll(dir, os.ModePerm); errMkDir != nil {
+		return nil, fmt.Errorf("could not create directory for new file: %w", errMkDir)
+	}
+
 	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 }
--- storage/openers_test.go
+++ storage/openers_test.go
@@ -30,6 +30,70 @@ func TestFileOpener_OpenCreate(t *testing.T) {
 	require.Equal(t, "baba is you", string(data))
 }
 
+func TestFileOpener_OpenCreate_DirectoryNotExisting_DirectoryIsCreated(t *testing.T) {
+	opener := storage.FileOpener{}
+	path := filepath.Join(os.TempDir(), "some", "inner", "folder", "baba.test")
+
+	file, errCreate := opener.OpenCreate(path)
+	require.NoError(t, errCreate)
+
+	defer func() {
+		_ = file.Close()
+		_ = os.RemoveAll(filepath.Join(os.TempDir(), "some"))
+	}()
+
+	_, errWrite := io.WriteString(file, "baba is you")
+	require.NoError(t, errWrite)
+
+	data, errRead := os.ReadFile(path)
+
+	require.NoError(t, errRead)
+	require.Equal(t, "baba is you", string(data))
+}
+
+func TestFileOpener_OpenCreate_DirectoryExists_NoError(t *testing.T) {
+	opener := storage.FileOpener{}
+	path := filepath.Join(os.TempDir(), "some", "inner", "folder", "baba.test")
+	errDir := os.MkdirAll(filepath.Join(os.TempDir(), "some", "inner", "folder"), os.ModePerm)
+	require.NoError(t, errDir)
+	defer func() {
+		_ = os.RemoveAll(filepath.Join(os.TempDir(), "some"))
+	}()
+
+	file, errCreate := opener.OpenCreate(path)
+	require.NoError(t, errCreate)
+
+	defer func() {
+		_ = file.Close()
+	}()
+
+	_, errWrite := io.WriteString(file, "baba is you")
+	require.NoError(t, errWrite)
+
+	data, errRead := os.ReadFile(path)
+
+	require.NoError(t, errRead)
+	require.Equal(t, "baba is you", string(data))
+}
+
+func TestFileOpener_OpenCreate_DirectoryNotExisting_FileWithSameName_Error(t *testing.T) {
+	opener := storage.FileOpener{}
+	path := filepath.Join(os.TempDir(), "some", "inner", "folder", "baba.test")
+	errDir := os.MkdirAll(filepath.Join(os.TempDir(), "some", "inner"), os.ModePerm)
+	require.NoError(t, errDir)
+	defer func() {
+		_ = os.RemoveAll(filepath.Join(os.TempDir(), "some"))
+	}()
+
+	preFile, preErrCreate := os.OpenFile(filepath.Join(os.TempDir(), "some", "inner", "folder"), os.O_CREATE, os.ModePerm)
+	require.NoError(t, preErrCreate)
+	require.NoError(t, preFile.Close())
+
+	file, errCreate := opener.OpenCreate(path)
+	require.Error(t, errCreate)
+	require.Nil(t, file)
+}
+
 func TestFileOpener_OpenRead(t *testing.T) {
 	path := filepath.Join(os.TempDir(), "baba.test")
 
